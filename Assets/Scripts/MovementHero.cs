a74db22c45e43c2f166e3ebf9a9fc107c697d7b6
inesahrens <ahrens.ines@web.de>
2015-06-27 13:59:19 +0200

    Addit movement script

---
+++ Assets/Scripts/MovementHero.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementHero : MonoBehaviour {
+
+	public float force;
+	private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Movement(Vector3 direction) {
+		rigidbody.AddForce (force * direction);
+	}
+
+
+}
