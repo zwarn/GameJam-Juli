78998bb4aab022c584315953fb39b0486c61c4d1
zwarn <Michael.Budahn@gmail.com>
2015-06-27 20:44:10 +0200

    musicController

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MusicController : MonoBehaviour {
+
+	public List<AudioClip> clips;
+
+	void Start() {
+		this.GetComponent<AudioSource>().clip = clips[Random.Range (0, clips.Count)];
+	}
+}
