32559d8a2eb5a8c63431e47d67670ce4ee889d42
zwarn <Michael.Budahn@gmail.com>
2015-06-27 20:06:24 +0200

    disco script

---
+++ Assets/Scripts/DiscoScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiscoScript : MonoBehaviour {
+
+	private MeshRenderer renderer;
+	public Material mat1;
+	public Material mat2;
+
+	// Use this for initialization
+	void Start () {
+		Renderer = GetComponent<MeshRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Mathf.RoundToInt (Time.time) % 2 == 0) {
+			renderer.material = mat1;
+		} else {
+			renderer.material = mat2;
+		}
+
+	}
+}
