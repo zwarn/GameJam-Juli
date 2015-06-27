829e1af2c67e3aa8842b8d6e31f6bca18edbced1
inesahrens <ahrens.ines@web.de>
2015-06-27 19:06:15 +0200

    Kanten zwischen Kameras

---
+++ Assets/Scripts/Arrows.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrows : MonoBehaviour {
+
+	public GameObject arrowX;
+	public GameObject arrowY;
+	public GameObject arrowZ;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
