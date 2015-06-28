aa320635cb788bf23f1075e713ae1182ef0a5351
zwarn <Michael.Budahn@gmail.com>
2015-06-28 12:27:10 +0200

    gamejam presentation

---
+++ Assets/rotateback.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateback : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		transform.Rotate (new Vector3 (90, 0, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
